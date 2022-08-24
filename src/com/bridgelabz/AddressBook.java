83a8b3fe00ad70c12a3905338c20e8e8cb3be3b3
abhijeet-t09 <tripathi.abhijeet0996@gmail.com>
2022-08-24 13:20:17 +0530

    Add Contact To Address Book

---
+++ src/com/bridgelabz/AddressBook.java
@@ -0,0 +1,40 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class AddressBook {
+
+    Contacts contact = new Contacts();
+
+    void addContact(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter First Name: ");
+        contact.setFirstName(scanner.nextLine());
+
+        System.out.print("Enter Last Name: ");
+        contact.setLastName(scanner.nextLine());
+
+        System.out.print("Enter Phone Number: ");
+        contact.setPhoneNumber(scanner.nextLine());
+
+        System.out.print("Enter Email-ID: ");
+        contact.setEmail(scanner.nextLine());
+
+        System.out.print("Enter address: ");
+        contact.setAddress(scanner.nextLine());
+
+        System.out.print("Enter City: ");
+        contact.setCity(scanner.nextLine());
+
+        System.out.print("Enter State: ");
+        contact.setState(scanner.nextLine());
+
+        System.out.print("Enter Zip-code: ");
+        contact.setZip(scanner.nextLine());
+    }
+
+    Contacts displayContact(){
+        return contact;
+    }
+}
--- src/com/bridgelabz/Contacts.java
+++ src/com/bridgelabz/Contacts.java
@@ -14,63 +14,79 @@ public class Contacts {
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
-    public String getFirstName() {
-        return firstName;
-    }
-
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
-    public String getLastName() {
-        return lastName;
-    }
-
 
     public void setPhoneNumber(String  phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
-    public String  getPhoneNumber() {
-        return phoneNumber;
-    }
-
 
     public void setEmail(String emailID) {
         this.emailID = emailID;
     }
-    public String getEmail() {
-        return emailID;
-    }
-
 
     public void setAddress(String address) {
         this.address = address;
     }
-    public String getAddress() {
-        return address;
-    }
-
 
     public void setCity(String city) {
         this.city = city;
     }
-    public String getCity() {
-        return city;
-    }
-
 
     public void setState(String state) {
         this.state = state;
     }
-    public String getState() {
-        return state;
-    }
-
 
     public void setZip(String zipCode) {
         this.zipCode = zipCode;
     }
-    public String getZip() {
-        return zipCode;
+
+
+
+    public String getFirstName() {
+        return firstName;
     }
+//
+//    public String getLastName() {
+//        return lastName;
+//    }
+//
+//    public String  getPhoneNumber() {
+//        return phoneNumber;
+//    }
+//
+//    public String getEmail() {
+//        return emailID;
+//    }
+//
+//    public String getAddress() {
+//        return address;
+//    }
+//
+//    public String getCity() {
+//        return city;
+//    }
+//
+//    public String getState() {
+//        return state;
+//    }
+//    public String getZip() {
+//        return zipCode;
+//    }
+
+
+    @Override
+    public String toString() {
+        return "Contact Details [" +
+                "Name -> " + firstName + " " + lastName +
+                " | Phone Number -> " + phoneNumber +
+                " | Email-ID -> " + emailID +
+                " | Address -> " + address +
+                " | City -> " + city +
+                " | State -> " + state +
+                " | Zip-code -> " + zipCode + "]";
+    }
+
 }
