a7e2655cdc655382ffcbcb3b457e2b327786389d
abhijeet-t09 <tripathi.abhijeet0996@gmail.com>
2022-08-24 11:38:15 +0530

    Create Contact in Address Book

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -2,6 +2,25 @@ package com.bridgelabz;
 
 public class AddressBookMain {
     public static void main(String[] args) {
-        System.out.println("***| Address Book System |***");
+        System.out.println("***| Address Book System |***\n");
+
+        Contacts contact = new Contacts();
+
+        contact.setFirstName("Abhijeet");
+        contact.setLastName("Tripathi");
+        contact.setAddress("E-45W");
+        contact.setCity("New Delhi");
+        contact.setState("Delhi");
+        contact.setZip("110087");
+        contact.setPhoneNumber("(+91) 8374738273");
+        contact.setEmail("abhijeet@bridgelabz.com");
+
+        System.out.println(contact.getFirstName() + " " +contact.getLastName());
+        System.out.println(contact.getPhoneNumber());
+        System.out.println(contact.getEmail());
+        System.out.println(contact.getAddress() + ",");
+        System.out.println(contact.getCity() + ", " + contact.getState());
+        System.out.println(contact.getZip());
+
     }
 }
