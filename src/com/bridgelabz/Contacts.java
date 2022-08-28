0bf9093ba42f3e912e09968a01e4a13349a3021d
abhijeet-t09 <tripathi.abhijeet0996@gmail.com>
2022-08-28 22:25:33 +0530

    Multiple Contacts in Address Book

--- src/com/bridgelabz/Contacts.java
+++ src/com/bridgelabz/Contacts.java
@@ -11,75 +11,67 @@ public class Contacts {
     private String phoneNumber;
     private String emailID;
 
+
     public void setFirstName(String firstName) {
         this.firstName = firstName;
+    }
+    public String getFirstName() {
+        return firstName;
     }
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
+    public String getLastName() {
+        return lastName;
+    }
 
     public void setPhoneNumber(String  phoneNumber) {
         this.phoneNumber = phoneNumber;
+    }
+    public String  getPhoneNumber() {
+        return phoneNumber;
     }
 
     public void setEmail(String emailID) {
         this.emailID = emailID;
     }
+    public String getEmail() {
+        return emailID;
+    }
 
     public void setAddress(String address) {
         this.address = address;
+    }
+    public String getAddress() {
+        return address;
     }
 
     public void setCity(String city) {
         this.city = city;
     }
+    public String getCity() {
+        return city;
+    }
 
     public void setState(String state) {
         this.state = state;
+    }
+    public String getState() {
+        return state;
     }
 
     public void setZip(String zipCode) {
         this.zipCode = zipCode;
     }
-
-
-
-    public String getFirstName() {
-        return firstName;
+    public String getZip() {
+        return zipCode;
     }
-
-//    public String getLastName() {
-//        return lastName;
-//    }
-//
-//    public String  getPhoneNumber() {
-//        return phoneNumber;
-//    }
-//
-//    public String getEmail() {
-//        return emailID;
-//    }
-//
-//    public String getAddress() {
-//        return address;
-//    }
-//
-//    public String getCity() {
-//        return city;
-//    }
-//
-//    public String getState() {
-//        return state;
-//    }
-//    public String getZip() {
-//        return zipCode;
-//    }
 
 
     @Override
     public String toString() {
-        return "Contact Details [" +
+        return "\nContact Details [" +
                 "Name -> " + firstName + " " + lastName +
                 " | Phone Number -> " + phoneNumber +
                 " | Email-ID -> " + emailID +
@@ -88,5 +80,19 @@ public class Contacts {
                 " | State -> " + state +
                 " | Zip-code -> " + zipCode + "]";
     }
+
+
+//    public Contacts (String firstName, String lastName, String address, String city, String state, String zipCode, String phoneNumber, String emailID){
+//        this.firstName = firstName;
+//        this.lastName = lastName;
+//        this.address = address;
+//        this.city = city;
+//        this.state = state;
+//        this.zipCode = zipCode;
+//        this.phoneNumber = phoneNumber;
+//        this.emailID = emailID;
+//    }
+//
+//    public Contacts(){}
 
 }
